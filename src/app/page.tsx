fff56f7a4b693b126cdaa225fcf78706b2ccda8f
Haydn Comley <haydn.comley@actually.software>
2026-03-16 19:41:35 +0000

    remove suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import { Suspense } from 'react';
-
-import { Spinner } from '~/components/spinner';
-
 import { HomePage } from './home-page';
 
 export default function Page() {
-	return (
-		<Suspense fallback={<Spinner />}>
-			<HomePage />
-		</Suspense>
-	);
+	return <HomePage />;
 }
